c35e3d9c48a28cf2107d345b20c081568c4aa316
rvazidnazm <rvazidnazm@gmail.com>
2025-01-30 23:07:56 +0700

    Update server.py

--- server.py
+++ server.py
@@ -27,6 +27,4 @@ def run_bot():
 if __name__ == "__main__":  
     threading.Thread(target=keep_alive, daemon=True).start()  # Keep-alive thread
     threading.Thread(target=run_bot, daemon=True).start()  # Thread untuk bot Telegram
-    
-    port = int(os.environ.get("PORT", 5000))  
-    app.run(host="0.0.0.0", port=port)  # Jalankan Flask
+    # Jangan panggil app.run() di sini
