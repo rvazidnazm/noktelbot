ab2133b0ca6098b8324e3116bb76833a94ef0263
rvazidnazm <rvazidnazm@gmail.com>
2025-01-30 10:18:42 +0700

    Update server.py

--- server.py
+++ server.py
@@ -1,12 +1,12 @@
-from flask import Flask
-import os
+from flask import Flask  
+import os  
 
-app = Flask(__name__)
+app = Flask(__name__)  
 
-@app.route('/')
-def home():
-    return "Bot Telegram Aktif!"
+@app.route('/')  
+def home():  
+    return "Bot Telegram Aktif!"  
 
-if name == "main":
-    port = int(os.environ.get("PORT", 5000))
+if __name__ == "__main__":  # Perbaikan di sini
+    port = int(os.environ.get("PORT", 5000))  
     app.run(host="0.0.0.0", port=port)
