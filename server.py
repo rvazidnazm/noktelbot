dc14d12d372d069744eb66c5b7553d1961394fb1
rvazidnazm <rvazidnazm@gmail.com>
2025-01-30 10:04:18 +0700

    Create server.py

---
+++ server.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import os
+
+app = Flask(name)
+
+@app.route('/')
+def home():
+    return "Bot Telegram Aktif!"
+
+if name == "main":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
